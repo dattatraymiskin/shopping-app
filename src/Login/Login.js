f6cab906d2f5953a31dc5b0a0429db3f9dd6858f
dattatraymiskin <dattatraymiskin0101@gmail.com>
2023-07-19 14:30:21 +0530

    login

---
+++ src/Login/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+   
+
+    return (  <div className="login container">
+
+        <div className="row">
+        <h1>Online Shopping cart</h1>
+        </div>
+        <div className="row">
+            <div className="col rowHight">
+            <button class="btn btn-success">LoginIn</button>
+            </div>
+            <div className="col">
+                <button class="btn btn-primary">SignUp</button>
+            
+            </div>
+        </div>
+    
+
+  </div>
+   );
+}
+
+export default Login;
